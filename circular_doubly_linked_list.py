66ca39c47a0475161fe58bb4b95fda80a046951c
gbstorey <gbstorey@asu.edu>
2022-10-27 16:38:45 -0700

    Completed circular doubly linked list module

--- circular_doubly_linked_list.py
+++ circular_doubly_linked_list.py
@@ -4,7 +4,7 @@ class Node:
         self.next = None #O(1)
         self.prev = None
 
-class DoublyLinkedList:
+class CircularDoublyLinkedList:
     def __init__(self):
         self.head = None #O(1)
         self.tail = None #O(1)
@@ -13,59 +13,63 @@ class DoublyLinkedList:
         while node:
             yield node
             node = node.next
+            if node == self.tail.next:
+                break
     #O(1) time and space complexity
-    def createDLL(self, nodeValue):
+    def createCDLL(self, nodeValue):
         node = Node(nodeValue)
-        node.prev = None
-        node.next = None
         self.head = node
         self.tail  = node
+        node.prev = node
+        node.next = node
         return "The DLL is created successfully."
     #O(n) time complexity, O(1) space complexity
-    def insertNode(self, value, location):
+    def insertNode(self, nodeValue, location):
         if self.head is None:
             print("The node cannot be inserted")
         else:
-            newNode = Node(value)
-
+            newNode = Node(nodeValue)
             if location == 0:
-                newNode.prev = None
                 newNode.next = self.head
+                newNode.prev = self.tail
                 self.head.prev = newNode
                 self.head = newNode
-                
+                self.tail.next = newNode
             elif location == -1:
-                newNode.next = None
+                newNode.next = self.head
                 newNode.prev = self.tail
-                self.tail.next=newNode
+                self.head.prev = newNode
+                self.tail.next = newNode
                 self.tail = newNode
-
             else:
-                traversalNode = self.head
+                tempNode = self.head
                 index = 0
-                while index < location-1:
-                    traversalNode = traversalNode.next
+                while index < location - 1:
+                    tempNode = tempNode.next
                     index += 1
-
-                nextNode = traversalNode.next
-                nextNode.prev = newNode
-                newNode.next = nextNode
-                newNode.prev = traversalNode
-                traversalNode.next = newNode
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
+            return "The node has been successfully inserted"
     #O(n) time complexity, O(1) space complexity
-    def traverseDLL(self):
-        node = self.head
-        while node:
-            print(node.value)
-            node = node.next
+    def traverseCDLL(self):
+        tempNode = self.head
+        while tempNode:
+            print(tempNode.value)
+            if tempNode== self.tail:
+                break
+            tempNode = tempNode.next
+     #O(n) time complexity, O(1) space complexity
+    def reverseTraverseCDLL(self):
+        tempNode = self.tail
+        while tempNode:
+            print(tempNode.value)
+            if tempNode == self.head:
+                break
+            tempNode = tempNode.prev
     #O(n) time complexity, O(1) space complexity
-    def reverseTraverseDLL(self):
-        node = self.tail
-        while node:
-            print(node.value)
-            node = node.prev
-    #O(n) time complexity, O(1) space complexity
-    def searchDLL(self, nodeValue):
+    def searchCDLL(self, nodeValue):
         if self.head is None:
             return "There is no node in the DLL."
         else:
@@ -74,40 +78,46 @@ class DoublyLinkedList:
             while tempNode:
                 if tempNode.value == nodeValue:
                     return f"{nodeValue} was found at node {index}"
+                elif tempNode == self.tail:
+                    return "The value does not exist in the CDLL."
                 else:
                     tempNode = tempNode.next
                     index += 1
-            return "The value does not exist in the DLL"
     #O(n) time complexity, O(1) space complexity
     def deleteNode(self, location):
         if self.head == None:
-            print("There is no node to delete")
+            print("There are no nodes in the CDLL.")
         else:
             if location == 0:
                 if self.head == self.tail:
-                    self.head = None
-                    self.tail = None
-                else: 
-                    self.head = self.head.next
                     self.head.prev = None
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None  
+                else:
+                    self.head = self.head.next
+                    self.head.prev = self.tail
+                    self.tail.next = self.head
             elif location == -1:
                 if self.head == self.tail:
+                    self.head.prev = None
+                    self.head.next = None
                     self.head = None
-                    self.tail = None
-                else: 
+                    self.tail = None  
+                else:
                     self.tail = self.tail.prev
-                    self.tail.next = None
-            else:
+                    self.tail.next = self.head
+                    self.head.prev = self.tail
+            else: 
                 tempNode = self.head
                 index = 0
                 while index < location - 1:
                     tempNode = tempNode.next
-                    index += 1
+                    index +=1
                 tempNode.next = tempNode.next.next
                 tempNode.next.prev  = tempNode
-            print("The node has been deleted.")
     #O(n) time complexity, O(1) space complexity
-    def deleteDLL(self):
+    def deleteCDLL(self):
         if self.head is None:
             print("There are no nodes in this DLL.")
         else:
@@ -117,16 +127,15 @@ class DoublyLinkedList:
                 tempNode = tempNode.next
             self.head = None
             self.tail = None
-            print("The DLL has been successfully deleted.")
+            print("The CDLL has been successfully deleted.")
 
-DLL = DoublyLinkedList()
-DLL.createDLL(5)
-DLL.insertNode(4, 0)
-DLL.insertNode(6, -1)
-DLL.insertNode(5.5, 2)
-DLL.traverseDLL()
-DLL.reverseTraverseDLL()
-print(DLL.searchDLL(5.5))
-print(DLL.searchDLL(70))
-DLL.deleteNode(0)
-DLL.traverseDLL()
+CDLL = CircularDoublyLinkedList()
+print(CDLL.createCDLL(5))
+print(CDLL.insertNode(4, 0))
+print(CDLL.insertNode(6, -1))
+print(CDLL.insertNode(5.5, 2))
+CDLL.traverseCDLL()
+print(CDLL.searchCDLL(5))
+print(CDLL.searchCDLL(70))
+# CDLL.reverseTraverseCDLL()
+# print([node.value for node in CDLL])
